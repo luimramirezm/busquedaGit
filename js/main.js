a97f2a6bbb92f76c3ee57831b45439bba6f72895
luimramirezm <luiramirezm@gmail.com>
2022-05-31 18:18:42 -0500

    con favoritos

--- js/main.js
+++ js/main.js
@@ -8,6 +8,19 @@ const ella = Vue.createApp({
             busqueda: null,  //agregamos esto y mandamos el input del formulario donde esta el texto 
             resultado: null, //null es vacío
             fail: null, 
+            favoritos: new Map() //Donde guardamos los favoritos haremos un mapa
+        }
+     },
+
+     computed:{
+         //Es para poder eliminar en el apartado de eliminar del html
+        esFavorito(){
+             return this.favoritos.has(this.resultado.id);
+         },
+        //con esta propiedad computada se retornan todos los favoritos
+        //pero unicamente los valores, no las key para eso se usa value()
+        TodosFavoritos(){
+            return Array.from(this.favoritos.values())
         }
      },
      methods: { //La palabra methods hace q  todo sean funciones entoces no se pone funtion
@@ -30,6 +43,25 @@ const ella = Vue.createApp({
                 this.busqueda = null
             }
             
+        },
+
+        addFavorito(){
+            //La clave de este map es el id y el resultado es el valor
+            this.favoritos.set(this.resultado.id, this.resultado);
+            this.UpdateStorage() //Aqui estamos llamando al método UpdateStorage
+        },
+
+        RemoverFavorito(){
+            //vamos a eliminar con el id
+            this.favoritos.delete(this.resultado.id);
+            this.UpdateStorage() //Aqui llama al metodo para actualizar lo q se guardo para saber si hay más o menos favoritos guardados
+        },
+
+        UpdateStorage(){
+            //localStorage te permite acceder al objeto local Storage; los datos persisten almacenados entre de las diferentes sesiones de navegación
+            //Se pretende guardar en caché la información de los favoritos de manera persistente
+            //JSON.stringify convierte todo lo q hay en el TodosFavoritos, en una cadena de texto y toca convertirlo asi porq el localStorage solo lee String
+            window.localStorage.setItem('favoritos', JSON.stringify(this.TodosFavoritos));
         }
      }
 
